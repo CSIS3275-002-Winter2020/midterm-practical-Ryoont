4f554b3d79565edce527d1f0865cbff7687f7e7a
ryoont <taherianarakir@student.douglascollege.ca>
2020-02-14 13:39:20 -0800

    Added java class with name and email

---
+++ Winter2020CSIS3275Midterm/src/HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+
+		public static void main(String[] args) {
+			System.out.println("Ryan Taheri");
+			System.out.println("taherianarakir@student.douglascollege.ca");
+		}
+}
